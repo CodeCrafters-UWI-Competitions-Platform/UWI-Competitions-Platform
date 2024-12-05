84b79c2936dcc4ab5c9b3bd2dac4c618dc00459c
AneesW03 <aneeswahid03@gmail.com>
2024-12-04 23:57:06 -0400

    Modifying config to connect database

--- App/default_config.py
+++ App/default_config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgresql://competitions_platform_db_caxy_user:f0BucNPkM500o0fXCJHasKyWWoB9EPmC@dpg-ct3ac4ggph6c73brpc5g-a/competitions_platform_db_caxy"
 SECRET_KEY = "secret key"
 JWT_ACCESS_TOKEN_EXPIRES = 7
-ENV = "DEVELOPMENT"
+ENV = "PRODUCTION"
